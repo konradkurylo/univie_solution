ff239c1faff1d70cb53dc2e74f9cfb5788481316
konradkurylo <konradkurylo@protonmail.com>
2024-09-18 20:55:51 +0200

    add one more sad path test + file rename

--- src/test/java/at/ac/univie/LibraryPublicApiLoadSummaryTest.java
+++ src/test/java/at/ac/univie/LibraryPublicApiLoadSummaryTest.java
@@ -3,7 +3,7 @@ package at.ac.univie;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class LibraryPublicApiTest {
+class LibraryPublicApiLoadSummaryTest {
 
 	@Test
 	void testLoadSummaryHappyPath() {
@@ -16,13 +16,23 @@ class LibraryPublicApiTest {
 	}
 
 	@Test
-	void testLoadSummarySadPath() {
+	void testLoadSummarySadPathNonExistingJson() {
 		// given
 		String source = "/non-existing.json";
 		// when
 		LibraryIssueWithLoadingDataException exception = org.junit.jupiter.api.Assertions.assertThrows(LibraryIssueWithLoadingDataException.class, () -> LibraryPublicApi.loadSummary(source));
 		// then
 		Assertions.assertThat(exception.getMessage()).isEqualTo("Issue while loading summary from source: /non-existing.json");
+	}
+
+	@Test
+	void testLoadSummarySadPathWrongJson() {
+		// given
+		String source = "src/test/resources/laps.json";
+		// when
+		LibraryIssueWithLoadingDataException exception = org.junit.jupiter.api.Assertions.assertThrows(LibraryIssueWithLoadingDataException.class, () -> LibraryPublicApi.loadSummary(source));
+		// then
+		Assertions.assertThat(exception.getMessage()).isEqualTo("Issue while loading summary from source: src/test/resources/laps.json");
 	}
 
 	private SummaryInput summaryInputResult(){
